b4505dedfdd4fbd9a782cdb49b0ca6e3953f6504
sandeep kumar <138839111+sandeepkumar3869@users.noreply.github.com>
2023-12-29 09:51:18 +0530

    Add files via upload

---
+++ home/urls.py
@@ -0,0 +1,23 @@
+
+#home
+# from django.urls import path
+# from .views import Home
+# from django.conf import settings
+# from django.conf.urls.static import static
+
+# urlpatterns = [
+#     path('',Home.as_view(),name="home"),
+
+# ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+from django.urls import path
+from .views import Home
+from django.conf import settings
+from django.conf.urls.static import static
+
+app_name = 'home'
+urlpatterns = [
+    path('', Home.as_view(), name="home"),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
